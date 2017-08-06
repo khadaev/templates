2c7d064afadc99901c06558987e3189623e8abc6
khadaev <khadaev98@gmail.com>
2017-08-06 17:26:05 +0300

    kuhn's algorithm

---
+++ kuhn.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace Kuhn {
+    int n, k;
+    vector<vector<int>> g;
+    vector<int> mt;
+    vector<char> used;
+
+
+    bool try_kuhn (int v) {
+        if (used[v])
+            return false;
+        used[v] = true;
+        for (int i = 0; i < (int)g[v].size(); ++i) {
+            int to = g[v][i];
+            if (mt[to] == -1 || try_kuhn(mt[to])) {
+                mt[to] = v;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void init(int _n, int _k) {
+        n = _n, k = _k;
+        g.resize(n);
+        mt.assign(k, -1);
+    }
+
+    void add_edge(int i, int j) {
+        g[i].push_back(j);
+    }
+
+    int run_kuhn() {
+        int ans = 0;
+        for (int v = 0; v < n; ++v) {
+            used.assign(n, false);
+            if (try_kuhn(v)) ++ans;
+        }
+        return ans;
+    }
+
+    void clear() {
+        g.clear();
+        mt.clear();
+        used.clear();
+    }
+}
+using Kuhn::add_edge;
+using Kuhn::run_kuhn;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+}
